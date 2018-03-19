9c0229fdc1f77bd9adaba3dd0f113c1ae46b21fa
bentocin <bentocin@users.noreply.github.com>
2018-03-19 07:02:54 +0100

    Add RMSE implementation

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,35 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  // Calculate the RMSE here.
+  VectorXd rmse(4);
+  rmse.fill(0.0);
+
+  // Sanity checks before calculation
+  if (estimations.size() != ground_truth.size())
+  {
+    std::cout << "ERROR: Estimations and ground truth have different length" << endl;
+    return rmse;
+  }
+
+  if (estimations.size() == 0)
+  {
+    std::cout << "ERROR: No values in estimations" << endl;
+    return rmse;
+  }
+
+  // Calculate sum of squared residuals
+  for (int i = 0; i < estimations.size(); i++)
+  {
+    // Calculate error and add square to the sum
+    VectorXd residual = estimations[i] - ground_truth[i];
+    rmse += residual.array() * residual.array();
+  }
+
+  // Average and root of the squared errors
+  rmse /= estimations.size();
+  rmse = rmse.array().sqrt();
+
+  return rmse;
+
 }
